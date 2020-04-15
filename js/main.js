a60d70df354d1360ba22f4da5149cc5d670277f4
will <will.alvarez@gmail.com>
2020-04-14 23:42:50 -0500

    init

---
+++ js/main.js
@@ -0,0 +1,29 @@
+var sheetUrl = 'https://spreadsheets.google.com/feeds/cells/18DmyWGlrJ8wPe-Ld3a7EwkpvxC9ITLKHRIjWUzpm5Ps/1/public/full?alt=json';
+var boletos = [];
+$.getJSON(sheetUrl, function (data) {
+    console.log(data);
+    var entry = data.feed.entry;
+    // the leftmost column of the Google Sheets
+
+
+    for (var i = 0; i < entry.length; i += 2) {
+        // entry[i].content.$t retrieves the content of each cell
+        boletos.push({ numero: entry[i].content.$t, nombre: entry[i + 1].content.$t })
+
+
+    }
+    for (const element of boletos) {
+        console.log(element);
+        var blocked = '';
+        if (element.nombre !== '-') {
+            blocked = 'active';
+        }
+        $('#grid').append('<div class="col-2"> <div class="numero" id="num-00"> <p>' + element.numero + '</p> <div class="blocked ' + blocked + '"></div> </div> </div>')
+    }
+
+
+})
+
+
+
+
